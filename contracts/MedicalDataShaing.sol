56094636052b3f31d29e73d85d67a92e4ec24205
Mac199 <xuheng24@gmail.com>
2019-02-13 14:57:20 -0500

    first commit

---
+++ contracts/MedicalDataShaing.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.24;
+
+/**
+ * The Mds contract does this and that...
+ */
+contract Mds {
+		
+}
---
+++ src/js/app.js
@@ -0,0 +1,80 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: async function() {
+    
+    App.web3Provider = web3.currentProvider;
+    web3 = new Web3(web3.currentProvider);
+
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.current_account = account;
+        //$("#current_user").html(App.current_account);
+        console.log(account);
+      }
+    });
+
+    // Load pets.
+    $.getJSON('../pets.json', function(data) {
+      var petsRow = $('#petsRow');
+      var petTemplate = $('#petTemplate');
+
+      for (i = 0; i < data.length; i ++) {
+        petTemplate.find('.panel-title').text(data[i].name);
+        petTemplate.find('img').attr('src', data[i].picture);
+        petTemplate.find('.pet-breed').text(data[i].breed);
+        petTemplate.find('.pet-age').text(data[i].age);
+        petTemplate.find('.pet-location').text(data[i].location);
+        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+
+        petsRow.append(petTemplate.html());
+      }
+    });
+
+    return await App.initWeb3();
+  },
+
+  initWeb3: async function() {
+    /*
+     * Replace me...
+     */
+
+    return App.initContract();
+  },
+
+  initContract: function() {
+    /*
+     * Replace me...
+     */
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '.btn-adopt', App.handleAdopt);
+  },
+
+  markAdopted: function(adopters, account) {
+    /*
+     * Replace me...
+     */
+  },
+
+  handleAdopt: function(event) {
+    event.preventDefault();
+
+    var petId = parseInt($(event.target).data('id'));
+
+    /*
+     * Replace me...
+     */
+  }
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
